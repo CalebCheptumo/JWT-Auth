9ccb399d1ff83f337a3dc9046347c3aedd4e95b0
calebcheptumo <ckibusia@gmail.com>
2023-12-18 15:36:56 +0300

    Create logout view & URL

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework import permissions , status
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken    
 
 class HomeView (APIView):
     authentication_classes = [JWTAuthentication]
@@ -13,3 +14,17 @@ class HomeView (APIView):
             'message': 'Hello, Welcome to the Home Page.'       
         }
         return Response(content)
+    
+class LogoutView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self, request, format=None):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
