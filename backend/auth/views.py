4020b6f94db2368be2a2d9af52be04fc0a87a0f2
calebcheptumo <ckibusia@gmail.com>
2023-12-18 15:23:38 +0300

    Create testing view

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+class HomeView (APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),
+            'message': 'Hello, Welcome to the Home Page.'       
+        }
+        return Response(content)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
 
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/',jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('', include('auth.urls')),
 ]
